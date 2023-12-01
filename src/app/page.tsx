19f02d947ec198bbdb7b66e3d52f46b0184ae148
takashi823 <uchimura.takashi724@gmail.com>
2023-12-02 01:59:31 +0900

    add:デザイン追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
+    <div className="flex min-h-screen items-center justify-center">
       <Image
+        className="rounded-full"
         src="/profile/profile.jpg"
         alt="プロフィール画像"
         width={200}
         height={200}
       />
-      <div>
-        <div>T.U</div>
-        <div>自己紹介</div>
-        <div>
+      <div className="flex flex-col gap-3 ml-10">
+        <div className="text-center text-5xl">T.U</div>
+        <div className="text-left">自己紹介</div>
+        <div className="leading-lg">
           初めまして。
           <br />
           エンジニアとしては、今年で7年目になります。
@@ -21,10 +22,10 @@ export default function Home() {
           <br />
           よろしくお願い致します。
           <br />
-          <div>
-            <div>趣味</div>
-            <div>お仕事</div>
-          </div>
+        </div>
+        <div className="flex gap-5">
+          <div className="flex justify-center items-center text-center box-border h-20 w-20 rounded-full shadow-xl bg-yellow-300 hover:bg-yellow-200">趣味</div>
+          <div className="flex justify-center items-center text-center box-border h-20 w-20 rounded-full shadow-xl bg-red-300 hover:bg-red-200">お仕事</div>
         </div>
       </div>
     </div>
