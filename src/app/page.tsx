1a0b4144af143bb4ced560a279c57a3fa6f8c023
takashi823 <uchimura.takashi724@gmail.com>
2023-12-02 01:10:54 +0900

    add:自己紹介文追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,32 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
-      <Image src="/profile/profile.jpg" alt="プロフィール画像" width={200} height={200} />
-      <div>Hello</div>
+      <Image
+        src="/profile/profile.jpg"
+        alt="プロフィール画像"
+        width={200}
+        height={200}
+      />
+      <div>
+        <div>T.U</div>
+        <div>自己紹介</div>
+        <div>
+          初めまして。
+          <br />
+          エンジニアとしては、今年で7年目になります。
+          <br />
+          主に、Java,Python,Typescriptでの開発経験があります。
+          <br />
+          よろしくお願い致します。
+          <br />
+          <div>
+            <div>趣味</div>
+            <div>お仕事</div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
